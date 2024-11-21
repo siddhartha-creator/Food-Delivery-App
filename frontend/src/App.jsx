3141066b232f3535342ec75e2a01efa3ea264378
siddhartha-creator <khatrisiddhartha453@gmail.com>
2024-11-21 10:18:10 +0545

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,23 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home/Home'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Cart from './pages/Cart/Cart'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path ='/' element={<Home />}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<PlaceOrder/>}/>
-      </Routes>
-    </div>
+    <>
+      <div className='app'>
+        <Navbar/>
+        <Routes>
+          <Route path ='/' element={<Home />}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/order' element={<PlaceOrder/>}/>
+        </Routes>
+      </div>
+
+      <Footer/>
+
+    </>
   )
 }
 
