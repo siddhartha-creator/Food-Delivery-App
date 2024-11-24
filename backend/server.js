9995cf56cf6ca0782ea7192d36c3c7ec1fe3f198
siddhartha-creator <khatrisiddhartha453@gmail.com>
2024-11-24 10:55:47 +0545

    Updated

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
+
 
 //app config
 const app = express()
@@ -10,6 +13,8 @@ const port = 4000
 //middleware
 app.use(express.json())
 app.use(cors())
+app.use("/api/user",userRouter)
+
 
 //DB Connection
 connectDB();
