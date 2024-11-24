58a44a56446742ff7935b9c2ec13722ef852c94b
siddhartha-creator <khatrisiddhartha453@gmail.com>
2024-11-24 09:44:23 +0545

    Update List.jsx

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -5,9 +5,9 @@ import axios from "axios"
 import {toast} from "react-toastify"
 import { useEffect } from 'react'
 
-const List = () => {
+const List = ({url}) => {
 
-const url = "http://localhost:4000"
+
   const [list,setList] = useState([]);
 
   const fetchList = async () => {
