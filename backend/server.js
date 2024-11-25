8758545d9a66d358d6bf30b99fe694669a36e3f4
siddhartha-creator <khatrisiddhartha453@gmail.com>
2024-11-25 10:20:18 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
 
 
 //app config
@@ -14,6 +15,7 @@ const port = 4000
 app.use(express.json())
 app.use(cors())
 app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
 
 
 //DB Connection
