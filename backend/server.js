e1f62bb853415df55ac8f8d11743b3eae4ad0ea1
siddhartha-creator <khatrisiddhartha453@gmail.com>
2024-11-25 14:15:23 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
 //app config
@@ -14,8 +15,7 @@ const port = 4000
 //middleware
 app.use(express.json())
 app.use(cors())
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
+
 
 
 //DB Connection
@@ -24,6 +24,9 @@ connectDB();
 //api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
 
 app.get('/',(req,res)=>{
     res.send("API Working")
