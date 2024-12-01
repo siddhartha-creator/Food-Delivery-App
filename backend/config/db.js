88fdaa607a4408e3dec7f5c36c6d235cc13a9f2b
siddhartha-creator <khatrisiddhartha453@gmail.com>
2024-12-01 12:36:48 +0545

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://khatrisiddhartha453:root123@cluster0.mdgfp.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('your api from mongodb').then(()=>console.log("DB Connected"));
 }
